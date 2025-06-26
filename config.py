696f79f7f57d358f59237a512e63c54fceaa3ee5
Ben Tinson <bentinson@Bens-MacBook-Pro.local>
2025-06-26 17:29:00 +0100

    Fix Heroku database dialect connection string

--- config.py
+++ config.py
@@ -6,11 +6,20 @@ UPLOAD_FOLDER = 'uploads'
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or '6238418573691154'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, UPLOAD_FOLDER)
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'avif'}
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+     # If the URL exists and starts with the old 'postgres://', replace it
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    # Set the final configuration variable
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or \
+        'sqlite:///' + os.path.join(basedir, 'site.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
     # Flask-Mail configuration
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.example.com')
